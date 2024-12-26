8422497a3deeab5ba4360fb5412ff960048d8d34
flanb <clement.renou.47@gmail.com>
2024-12-26 02:14:33 +0100

    feat: add custom cursor

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
 import Experience from 'core/Experience.js'
 
 const experience = new Experience(document.querySelector('canvas#webgl'))
+
+function customCursor() {
+	const cursor = document.querySelector('.cursor-container')
+
+	document.addEventListener('mousemove', (event) => {
+		cursor.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`
+	})
+}
+
+customCursor()
